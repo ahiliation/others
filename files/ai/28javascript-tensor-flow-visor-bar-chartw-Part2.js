4e2247f0aa3eaa775fb2033beda19dde5d7cb108
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-07-24 22:35:47 +0530

    server side scripting

---
+++ files/ai/28javascript-tensor-flow-visor-bar-chartw-Part2.js
@@ -0,0 +1,13 @@
+import * as tf from '@tensorflow/tfjs';
+
+const data = [
+  {index: 0, value: 100},
+  {index: 1, value: 200},
+  {index: 2, value: 150},
+  {index: 3, value: 250},
+];
+
+console.log(data[0]);
+
+// tfvis.render.barchart({name:"my Graphs"}, data);
+
