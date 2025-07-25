5c884adcc03407c32fccdc7ba17c09adbc027e35
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-07-25 23:56:32 +0530

    server side scripting

---
+++ files/ai/30javascript-tensor-flow-visor-line-graph-Part2.js
@@ -0,0 +1,12 @@
+import * as tf from '@tensorflow/tfjs';
+
+let values = [
+  {x: 1, y: 20},
+  {x: 2, y: 30},
+  {x: 3, y: 15},
+  {x: 4, y: 12}
+];
+
+console.log(values[0]);
+
+// tfvis.render.linechart({name: 'my Lines'}, {values});
