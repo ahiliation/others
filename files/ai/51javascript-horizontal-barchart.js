7a464e220c5e1ed674460ee494d35edc5dcdc502
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-07-28 21:51:45 +0530

    server side scripting related

---
+++ files/ai/51javascript-horizontal-barchart.js
@@ -0,0 +1,14 @@
+const xArray = [55, 49, 44, 24, 15];
+const yArray = ["Italy ", "France ", "Spain ", "USA ", "Argentina "];
+
+const data = [{
+  x:xArray,
+  y:yArray,
+  type:"bar",
+  orientation:"h",
+  marker: {color:"rgba(255,0,0,0.6)"}
+}];
+
+const layout = {title:"World Wide Wine Production"};
+
+console.log(data[0]);
