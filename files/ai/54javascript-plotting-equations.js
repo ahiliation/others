fc92656815bee1c0fca909e1061c7cb41e84aa79
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-07-30 20:22:51 +0530

    server side scripting related and data structure related

---
+++ files/ai/54javascript-plotting-equations.js
@@ -0,0 +1,14 @@
+let exp = "Math.sin(x)";
+
+// Generate values
+const xValues = [];
+const yValues = [];
+for (let x = 0; x <= 10; x += 0.1) {
+  xValues.push(x);
+  yValues.push(eval(exp));
+}
+
+// Display using Plotly
+const data = [{x:xValues, y:yValues, mode:"lines"}];
+const layout = {title: "y = " + exp};
+console.log(layout["title"])
