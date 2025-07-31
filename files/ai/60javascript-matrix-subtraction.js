a1a89507751907ccb54aac01fd0bb88619895203
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-08-01 01:43:46 +0530

    server side scripting related and matrix subtraction related

--- files/ai/60javascript-matrix-subtraction.js
+++ files/ai/60javascript-matrix-subtraction.js
@@ -8,7 +8,7 @@
 const mA = math.matrix([[1, 2], [3, 4], [5, 6]]);
 const mB = math.matrix([[1,-1], [2,-2], [3,-3]]);
 
-// console.log(mB.get([1][1]));
+// console.log(mB.get([1]));
 
 // Matrix Subtraction
 const matrixSub = math.subtract(mA, mB);
