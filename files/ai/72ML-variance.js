619bfda19c469502ac7676a03d917a7efc85d228
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-08-15 20:16:03 +0530

    work related

---
+++ files/ai/72ML-variance.js
@@ -0,0 +1,15 @@
+// Machine Learning
+// Calculate the Variance
+
+
+// Calulate the Mean (m)
+let m = (7+8+8+9+9+9+10+11+14+14+15)/11;
+
+// Calculate the Sum of Sqares (ss)
+let ss = (7-m)**2 + (8-m)**2 + (8-m)**2 + (9-m)**2 + (9-m)**2 + (9-m)**2 + (10-m)**2 + (11-m)**2 + (14-m)**2 + (14-m)**2 + (15-m)**2;
+
+// Calculate the Variance
+let variance = ss / 11;
+
+// Diplay the Variance
+console.log(variance);
