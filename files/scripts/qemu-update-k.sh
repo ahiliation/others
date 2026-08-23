09202465f7ddd29e79b7efca8333cb347a8bf3eb
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2026-08-23 18:24:17 +0530

    updated for checking if user has  root privilege

--- files/scripts/qemu-update-k.sh
+++ files/scripts/qemu-update-k.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Error: This script must be run as root." >&2
+    exit 1
+fi
+
+echo "Success: Running with root privileges."
+
 mkdir /tmp/rootfs
-sudo mount -o loop rootfs.img /tmp/rootfs
+mount -o loop rootfs.img /tmp/rootfs
 make modules_install INSTALL_MOD_PATH=/tmp/rootfs/ 
-sudo umount /tmp/rootfs
+umount /tmp/rootfs
