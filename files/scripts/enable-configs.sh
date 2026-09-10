ec486f8238486a6b554a017dbb70c156a93c7495
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2026-09-10 20:09:49 +0530

    file updated

--- files/scripts/enable-configs.sh
+++ files/scripts/enable-configs.sh
@@ -1,5 +1,11 @@
 # Enable Kernel Address Sanitizer (KASAN) for memory bug detection
 ./scripts/config --enable CONFIG_KASAN
 # Enable lockdep for deadlock detection
-./scripts/config --enable CONFIG_PROT_LOCKING
-./scripts/config --enable CONFIG_PROVE_LOCKING
+#./scripts/config --enable CONFIG_PROT_LOCKING
+#./scripts/config --ensble CONFIG_PROVE_LOCKING
+./scripts/config --enable CONFIG_KASAN_INLINE
+./scripts/config --enable CONFIG_CONFIGFS_FS
+./scripts/config --enable CONFIG_SECURITYFS
+
+./scripts/config --enable CONFIG_CMDLINE_BOOL
+./scripts/config --set-str CONFIG_CMDLINE "net.ifnames=0"
